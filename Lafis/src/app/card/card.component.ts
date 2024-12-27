ef9d00695f334a04df61ccc40bf07b6c21cfe0a6
alexandrendev <alxfn86@gmail.com>
2024-12-27 02:05:30 -0300

    feat: listagem de simulações

--- Lafis/src/app/card/card.component.ts
+++ Lafis/src/app/card/card.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { CommonModule } from '@angular/common';
+// import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
 
   id: string = 'id teste';
   apertureType: string = 'abertura';
   sourceType: string = 'fonte';
   emissions: number = 219093;
+  status: string = 'Rodando'
+
+  simulations:any[] = [];
+
+  constructor(private api: ApiService){}
+
+  ngOnInit(): void {    
+    this.api.getAllSimulations().subscribe(
+     (data)=>{
+      this.simulations = data;
+     },
+     (error) => {
+      console.error('Erro ao carregar simulações', error);  // Tratamento de erro
+    }
+    );
+
+    console.log(this.simulations);
+  }
+
 }
