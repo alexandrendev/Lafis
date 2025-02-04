280394fe04c51091ae85902432c88222752ae576
alexandrendev <alxfn86@gmail.com>
2025-02-04 12:07:19 -0300

    fix: home

--- Lafis/src/app/app.routes.ts
+++ Lafis/src/app/app.routes.ts
@@ -4,7 +4,6 @@ import { CardComponent } from './card/card.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'cadastro', component: CadastroSimulacaoComponent},
+    {path: '', component: CadastroSimulacaoComponent},
     {path: 'all', component: CardComponent}
 ];
