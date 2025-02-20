43352fb92f72e8fcb96c918db72f8ac1661fb50e
alexandrendev <alxfn86@gmail.com>
2025-02-20 16:58:42 -0300

    fix sidebar routes

--- Lafis/src/app/app.routes.ts
+++ Lafis/src/app/app.routes.ts
@@ -4,6 +4,6 @@ import { CardComponent } from './card/card.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    {path: '', component: CadastroSimulacaoComponent},
-    {path: 'all', component: CardComponent}
+    {path: 'new', component: CadastroSimulacaoComponent},
+    {path: '', component: CardComponent}
 ];
