9cf7e9b52ed771aba566bad3c04f7989f2a55219
alexandrendev <alxfn86@gmail.com>
2025-04-01 16:18:12 -0300

    fix: remove ssr

--- Lafis/src/app/app.config.ts
+++ Lafis/src/app/app.config.ts
@@ -3,14 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch()),
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([authInterceptor]),
+    ),
     [provideCharts(withDefaultRegisterables())]
   ]
 };
