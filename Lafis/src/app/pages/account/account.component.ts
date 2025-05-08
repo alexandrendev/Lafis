f95eed9865b973c7355eeb28ebc8fd79276c74ea
alexandrendev <alxfn86@gmail.com>
2025-05-08 09:19:17 -0300

    feat: account page styles

--- Lafis/src/app/pages/account/account.component.ts
+++ Lafis/src/app/pages/account/account.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { InfoItemComponent } from '../../components/info-item/info-item.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-account',
-  imports: [],
+  imports: [InfoItemComponent, CommonModule],
   templateUrl: './account.component.html',
   styleUrl: './account.component.scss'
 })
 export class AccountComponent {
+  created = '2023-01-01';
 
+  AccountComponent(){
+
+  }
+
+  public requestPasswordReset(){
+    alert('Senha esquecida? Clique aqui para redefinir a sua senha.');
+  }
 }
